f2dc63368448d8f48c8c39416f7d2c78f75b211a
DanilaMolchanov <sadme28@gmail.com>
2020-08-22 12:00:23 +0300

    Залил OpenCart

---
+++ system/storage/cache/96/9626fd9b83c5bfccda45dec5dfc7beb99f84797fc860de257c46a7332b3b1be2.php
@@ -0,0 +1,50 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_7dff8916b4ea7ce898861b780a99c6362273c873573400d08056344dfbe1502a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\">
+    <a href=\"http://opencart.ru\" target=\"_blank\">";
+        // line 3
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>
+  </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 3,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container">*/
+/*     <a href="http://opencart.ru" target="_blank">{{ text_project }}</a>*/
+/*   </div>*/
+/* </footer>*/
+/* </body></html>*/
---
+++ system/storage/cache/b9/b9db4877772b630edd9d98f8a297eed3e52cbc7a8d5d89a6d3bd19b0f9455a27.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_2f15ca8f92efd76abe5016841c24772bbe566f5e03ae68b90a8c6e9b8f308bd2 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
