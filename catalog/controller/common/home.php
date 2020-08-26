181274cc576e8b192027d97a83263ea9d6aa56d0
DanilaMolchanov <sadme28@gmail.com>
2020-08-26 15:23:37 +0300

    Добавил функционал на главную страницу

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -1,6 +1,8 @@
 <?php
-class ControllerCommonHome extends Controller {
-	public function index() {
+class ControllerCommonHome extends Controller
+{
+	public function index()
+	{
 		$this->document->setTitle($this->config->get('config_meta_title'));
 		$this->document->setDescription($this->config->get('config_meta_description'));
 		$this->document->setKeywords($this->config->get('config_meta_keyword'));
@@ -15,6 +17,33 @@ class ControllerCommonHome extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
+
+		$this->load->model('catalog/product');
+
+		$this->load->model('tool/image');
+
+		$products_darvin = $this->model_catalog_product->getNewForDarvin();
+
+		$this->log->write($products_darvin);
+
+		if ($products_darvin) {
+			foreach ($products_darvin as $result) {
+				if ($result['image']) {
+					$image = $this->model_tool_image->resize($result['image'], 200, 200);
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', 200, 200);
+				}
+
+				$data['products_darvin'][] = array(
+					'product_id'  => $result['product_id'],
+					'thumb'       => $image,
+					'name'        => $result['name'],
+					'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
+				);
+			}
+		}
+
+		$this->log->write($data['products_darvin']);
 
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
